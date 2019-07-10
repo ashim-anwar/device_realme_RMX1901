a61e17c3bd1eabd76b22adece7014b5298da7063
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-07-10 19:53:01 +0200

    RMX1901: Import stub camera motor HAL
    Change-Id: I014039b858e501c14020d1148175bad3d607b630
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -343,6 +343,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
+# camera Motor
+PRODUCT_PACKAGES += \
+    lineage.camera.motor@1.0-service.realme_sdm710
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl.realme_sdm710 \
