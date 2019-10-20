3d1a3d999ccec601869521f7a5ce6f3f17ce1509
Ethan Chen <intervigil@gmail.com>
2019-10-20 01:17:32 -0700

    RMX1901: Remove dead RCS packages
    Change-Id: Ie2c03fa11cb08c7ab900d7b666981d592aba19c7

--- device.mk
+++ device.mk
@@ -352,13 +352,6 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc \
     init.devicesetting.rc
 
-# RCS
-PRODUCT_PACKAGES += \
-    rcs_service_aidl \
-    rcs_service_aidl.xml \
-    rcs_service_api \
-    rcs_service_api.xml
-
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_RMX1901
