c3a909464c7284f80dc65fe09e07aebf275de0cc
hypercleric <kumaradisslbya@gmail.com>
2020-08-12 14:19:51 +0000

    RMX1901:Typo

--- device.mk
+++ device.mk
@@ -238,7 +238,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-xtended
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
