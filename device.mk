dev-harsh1998/android_device_realme_X2

x2: init: add permissions for devicesettings nodes

device.mk
@@ -53,7 +53,8 @@ PRODUCT_PACKAGES += \
 
 # Common init scripts
 PRODUCT_PACKAGES += \
-    init.qcom.rc
+    init.qcom.rc \
+    init.devicesetting.rc
 
 PRODUCT_PACKAGES += \
     fstab.qcom

ashim-anwar/device_realme_RMX1901

device.mk
@@ -323,7 +323,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.devicesetting.rc
 
 # RCS
 PRODUCT_PACKAGES += \
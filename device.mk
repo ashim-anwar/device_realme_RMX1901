914fe7e672864d474c4485de323d4007b8daa923
Harshit Jain <god@harsh1998.me>
2020-07-16 04:51:42 +0530

    x2: init: add permissions for devicesettings nodes

--- device.mk
+++ device.mk
@@ -323,7 +323,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.devicesetting.rc
 
 # RCS
 PRODUCT_PACKAGES += \
