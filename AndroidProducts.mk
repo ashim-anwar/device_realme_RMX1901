605c5564235bb4a03634231c70b99642444f67a4
hypercleric <kumaradisslbya@gmail.com>
2020-08-12 10:16:33 +0000

    RMX1901:Xtended

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1921.mk
+    $(LOCAL_DIR)/xtended_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_RMX1921-userdebug \
-    aosp_RMX1921-eng
+    xtended_RMX1901-userdebug \
+    xtended_RMX1901-eng
