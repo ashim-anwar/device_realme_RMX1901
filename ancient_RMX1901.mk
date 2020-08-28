79e41e69def516f8265fc93f24f3ed81d3a410c7
Sathamspot <sathamhussain.m11@gmail.com>
2020-08-28 10:42:35 +0000

    RMX1901: Update Coral August Fingerprint

--- ancient_RMX1901.mk
+++ ancient_RMX1901.mk
@@ -26,8 +26,8 @@ PRODUCT_NAME := ancient_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200705.002 6506677 release-keys" \
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys" \
     PRODUCT_NAME="RMX1901"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
+BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
