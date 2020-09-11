ab6bb54029ccb4f3bdacaa91a685e346f64b6c67
Harshit Jain <god@hyper-labs.tech>
2020-09-11 21:11:36 +0530

    RMX1901: switch to aosp telephony

--- device.mk
+++ device.mk
@@ -10,7 +10,7 @@ $(call inherit-product, build/make/target/product/product_launched_with_p.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/realme/RMX1901/RMX1901-vendor.mk)
