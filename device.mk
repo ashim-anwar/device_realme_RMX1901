3f054bfd7b0ef70e47b9a5bfd2594e09aa2ccb83
MadhavSaladi <MadhavSaladi@users.noreply.github.com>
2021-05-21 06:35:56 +0000

    RMX1901: Add GcamGo

--- device.mk
+++ device.mk
@@ -108,7 +108,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libstdc++.vendor \
-    Snap \
+    GcamGo \
     libgui_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
