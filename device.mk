Ij-israfil/cherish-13

RMX1901: Import sensors/2.0/multihal

device.mk
@@ -337,8 +337,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@2.0-service.multihal.nicky \
     libsensorndkbridge
 
 # Soong namespaces

ashim-anwar/device_realme_RMX1901

device.mk
@@ -305,6 +305,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service
 
 PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-service.multihal.x \
     libsensorndkbridge
 
 # Soong namespaces