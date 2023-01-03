Ij-israfil/cherish-13

RMX1901: Add fingerprint changes

device.mk
@@ -179,15 +179,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
-
-ifeq ($(TARGET_HAS_UDFPS),true)
-PRODUCT_PACKAGES += \
-    libudfpshandler
-
-PRODUCT_PACKAGES += \
-    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
-endif
+    android.hardware.biometrics.fingerprint@2.3-service.RMX1901 \
+    vendor.oppo.hardware.biometrics.fingerprint@2.1
 
 # Gatekeeper
 PRODUCT_PACKAGES += \

ashim-anwar/device_realme_RMX1901

device.mk
@@ -181,7 +181,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-
+    android.hardware.biometrics.fingerprint@2.3-service.x \
+    vendor.oppo.hardware.biometrics.fingerprint@2.1
 
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \