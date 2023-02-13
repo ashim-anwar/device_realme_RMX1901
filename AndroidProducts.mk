6f19dcf1e97aa8252077619dbde520f8a2cccbac
Shravankumar <109328274+shravansayz@users.noreply.github.com>
2023-02-13 12:30:46 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_RMX1901.mk
+    $(LOCAL_DIR)/octavi_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_RMX1901-userdebug \
-    spark_RMX1901-eng
+    octavi_RMX1901-userdebug \
+    octavi_RMX1901-eng
