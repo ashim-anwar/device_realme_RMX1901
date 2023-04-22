494e63682cac3caa858cf84721ad4e58fd4824cc
Aura-Subhadeep <subhadeepadhikari5@gmail.com>
2023-04-22 03:42:04 +0000

    RMX1901: Switch to AutoSingleLayer Android 13 Setting
    * According to https://source.android.com/docs/core/graphics/unsignaled-buffer-latch
    
    Signed-off-by: onettboots <blackcocopet@gmail.com>
    Signed-off-by: SageOfD6Path <mail2anirban95@gmail.com>

--- prop/vendor_prop.mk
+++ prop/vendor_prop.mk
@@ -174,6 +174,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true \
     ro.surface_flinger.protected_contents=true
+    debug.sf.auto_latch_unsignaled=true
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
