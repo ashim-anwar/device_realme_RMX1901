7ea4e1531223ef94367d3881dda3636e2f76c3b0
Aura-Subhadeep <subhadeepadhikari5@gmail.com>
2023-04-22 03:35:09 +0000

    RMX1901: perf: Enable the pre-rendering feature
     * Pre-rendering feature can improve FPS and reduce the janks
       in scrolling cases.

--- prop/vendor_prop.mk
+++ prop/vendor_prop.mk
@@ -193,6 +193,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so
+    ro.vendor.perf.scroll_opt=true
 
 # SoC
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
