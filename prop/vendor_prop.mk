53f690a4d07f6a3c585bed4fbb4ad12060485b89
qwertyuiii-code <soorprp@ya.ru>
2023-04-30 17:38:09 +0530

    RMX1901: Import more surface flinger flags
     - For better smoothness and to eliminate lags
     - QS animation, app opening & app switching seem to have improved.
    
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- prop/vendor_prop.mk
+++ prop/vendor_prop.mk
@@ -165,6 +165,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sdm.support_writeback=0 \
     ro.config.avoid_gfx_accel=true \
     ro.hwui.render_ahead=3 \
+    ro.surface_flinger.set_idle_timer_ms=80 \
+    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true \
     ro.surface_flinger.protected_contents=true
 
 # IMS
