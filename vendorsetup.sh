80ee7cbafc351d39a44c2516c89941a8a4995a57
AnilShebin <anilshebin@gmail.com>
2023-05-06 23:16:30 +0530

    RMX1901: Nuked Proton Clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,15 @@
 # Vendor Tree
-echo 'Cloning Vendor tree [1/5]'
+echo 'Cloning Vendor tree [1/4]'
 git clone git@github.com:ashim-anwar/vendor_realme_RMX1901-13.git -b 13 vendor/realme/RMX1901
 
 # Kernel Tree
-echo 'Cloning Kernel tree [2/5]'
+echo 'Cloning Kernel tree [2/4]'
 git clone git@github.com:RMX1901/kernel_realme_RMX1901.git -b x.319 kernel/realme/RMX1901
 
 # Realme In-Screen Proximity Patch
-echo 'Patching Realme In-Screen Proximity [3/5]'
+echo 'Patching Realme In-Screen Proximity [3/4]'
 cd frameworks/base && git fetch git@github.com:ashim-anwar/proximity_patch.git 13 && git cherry-pick 30ea88924980002d94fb8e2bf4a5016905ca6914 && cd ../..
 
-# Use Proton Clang For Compilation
-echo ' Cloning Proton Clang For Compilation [4/5]'
-git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
-
 # Inherit Pocket Mode
-echo ' Cloning PocketMode [5/5]'
+echo ' Cloning PocketMode [5/4]'
 git clone git@github.com:ashim-anwar/packages_apps_PocketMode.git -b 13 packages/apps/PocketMode
-
