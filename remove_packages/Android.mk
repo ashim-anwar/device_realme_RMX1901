85cfbb533f6bfe03ad2bce2edba1fcffbf23f441
ashim-anwar <ashim.anwar22@gmail.com>
2023-07-07 12:16:47 +0000

    RMX1901: Update remove_packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO DMService MyVerizonServices RecorderPrebuilt Maps YouTube Photos Drive PrebuiltGmail arcore SoundAmplifierPrebuilt SafetyHubPrebuilt OBDM_Permissions obdm_stub Showcase SprintDM SprintHM USCCDM VZWAPNLib VzwOmaTrigger AndroidAutoStubPrebuilt
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO DMService MyVerizonServices RecorderPrebuilt Maps YouTube Photos
+LOCAL_OVERRIDES_PACKAGES := Drive PrebuiltGmail arcore SoundAmplifierPrebuilt Safety OBDM_Permissions obdm_stub Showcase SprintDM SprintHM USCCDM
+LOCAL_OVERRIDES_PACKAGES := VZWAPNLib VzwOmaTrigger AndroidAutoStubPrebuilt GoogleTTS GoogleCamera
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
