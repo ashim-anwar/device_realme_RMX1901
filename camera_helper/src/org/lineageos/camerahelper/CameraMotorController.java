77870e8c0afa0aaf0ad081561febe1a2f699e9a4
Ij Israfil <israfilkhan494@gmail.com>
2023-12-03 22:00:56 +0600

    RMX1901: Fix camera motor path
    Signed-off-by: Ij-Israfil <israfilkhan494@gmail.com>

--- camera_helper/src/org/lineageos/camerahelper/CameraMotorController.java
+++ camera_helper/src/org/lineageos/camerahelper/CameraMotorController.java
@@ -28,16 +28,18 @@ public class CameraMotorController {
 
     // Camera motor paths
     private static final String CAMERA_MOTOR_ENABLE_PATH =
-            "/sys/devices/platform/vendor/vendor:motor_pl/enable";
+            "/sys/class/motor/enable";
     public static final String CAMERA_MOTOR_HALL_CALIBRATION =
-            "/sys/devices/platform/vendor/vendor:motor_pl/hall_calibration";
+            "/sys/class/motor/hall_calibration";
     private static final String CAMERA_MOTOR_DIRECTION_PATH =
-            "/sys/devices/platform/vendor/vendor:motor_pl/direction";
+            "/sys/class/motor/direction";
     private static final String CAMERA_MOTOR_POSITION_PATH =
-            "/sys/devices/platform/vendor/vendor:motor_pl/position";
+            "/sys/class/motor/position";
+
     // Motor calibration data path
     public static final String CAMERA_PERSIST_HALL_CALIBRATION =
             "/mnt/vendor/persist/engineermode/hall_calibration";
+            
     // Motor fallback calibration data
     public static final String HALL_CALIBRATION_DEFAULT =
             "170,170,480,0,0,480,500,0,0,500,1500";
