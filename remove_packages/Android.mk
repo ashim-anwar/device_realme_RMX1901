f8b47e97d72daea9c524d8b6e42052ac909eb138
ashim-anwar <ashim.anwar22@gmail.com>
2023-12-19 07:35:00 +0000

    RMX1901: Remove more gapps
    Signed-off-by: ashim-anwar <ashim.anwar22@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Drive Maps Photos MeetPrebuilt PrebuiltGmail GoogleCamera AmbientStreaming
+LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Drive Maps Photos MeetPrebuilt PrebuiltGmail GoogleCamera AmbientStreaming FM2 Papers AndroidAutoStubPrebuilt Showcase
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
